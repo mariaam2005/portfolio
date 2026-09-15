76595b63134eb39337addcfc27b0066c23d5cd64
Mariam Ahmed El-Sebaay <mariamahmedkamal2005@gmail.com>
2026-09-15 13:26:04 +0300

    Update learning experience , skills & contact

--- script.js
+++ script.js
@@ -6,3 +6,34 @@ if (menuToggle && navLinks) {
         navLinks.classList.toggle("show");
     });
 }
+
+const filterButtons = document.querySelectorAll(".filter-btn");
+const learningCards = document.querySelectorAll(".learning-card");
+
+filterButtons.forEach(button => {
+
+    button.addEventListener("click", () => {
+
+        const filter = button.getAttribute("data-filter");
+
+        filterButtons.forEach(btn => {
+            btn.classList.remove("active");
+        });
+
+        button.classList.add("active");
+
+        learningCards.forEach(card => {
+
+            const category = card.getAttribute("data-category");
+
+            if (filter === "all" || category === filter) {
+                card.classList.remove("hidden");
+            } else {
+                card.classList.add("hidden");
+            }
+
+        });
+
+    });
+
+});
